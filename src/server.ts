710be57165e7c7adc95af361f1a3eb7ebd15f7e0
umerwqr <umerwaqar154@gmail.com>
2025-01-06 04:32:35 +0500

    abdullah pharmacy

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/mongodbConnection";
+import baseRoute from "./router/index";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+baseRoute(app);
+app.get("/", (req: any, res: any) => {
+  res.send("Hello World");
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.message);
+  res.status(500).send("Internal Server Error");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 3000");
+  connectDB();
+});
